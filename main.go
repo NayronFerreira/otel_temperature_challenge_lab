8263662084b9ef42d1c4dbb6de86fce3260501f5
Nayronferreira <nayronferreiradev@gmail.com>
2024-04-06 22:59:03 -0300

    [OtelChallenge] - starting implementation of Open Telemetry

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
 
 	"github.com/NayronFerreira/otel_temperature_challenge_lab/config"
 	"github.com/NayronFerreira/otel_temperature_challenge_lab/internal/web"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
@@ -17,4 +27,50 @@ func main() {
 
 	web.InitializeRoutes(config)
 
+}
+
+func initProvider(serviceName, urlCollector string) (func(context.Context) error, error) {
+
+	ctx := context.Background()
+
+	resource, err := resource.New(ctx,
+		resource.WithAttributes(
+			semconv.ServiceNameKey.String(serviceName),
+		),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+
+	conn, err := grpc.DialContext(
+		ctx,
+		urlCollector,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	traceExporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithGRPCConn(conn))
+	if err != nil {
+		return nil, err
+	}
+
+	bsp := sdktrace.NewBatchSpanProcessor(traceExporter)
+	traceProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithResource(resource),
+		sdktrace.WithSpanProcessor(bsp),
+	)
+
+	otel.SetTracerProvider(traceProvider)
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+
+	return traceProvider.Shutdown, nil
 }
